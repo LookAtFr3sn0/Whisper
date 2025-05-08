594d9c20e23c97672c000f9d2dfa615a5b77974f
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-08 21:39:37 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: fileURLToPath(new URL('../backend/public', import.meta.url)),
+    emptyOutDir: true,
+    watch: {
+      include: ['src/**/*', 'index.html'],
+    }
+  }
+})
