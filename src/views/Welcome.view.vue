81d805bc85f867176e379e997d9f2e77e4970849
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-09 23:04:25 +0200

    Include email in registration verification request body

--- src/views/Welcome.view.vue
+++ src/views/Welcome.view.vue
@@ -115,7 +115,7 @@ const onRegister = async () => {
 		response = await fetch('/api/register/verify', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ token, registrationRecord }),
+			body: JSON.stringify({ email, token, registrationRecord }),
 		});
 		if (!response.ok) {
 			throw new Error('Network response was not ok ' + response.statusText);
