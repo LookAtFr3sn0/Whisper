7a400046b5c8f5292a64a27af56153da64e1ba02
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-09 16:55:10 +0200

    Add login card

--- src/views/Welcome.view.vue
+++ src/views/Welcome.view.vue
@@ -2,7 +2,7 @@
 import { onMounted, nextTick, ref } from 'vue';
 import gsap from 'gsap';
 
-const page                 = ref('signup');
+const page                 = ref('signin');
 const username 	           = ref('');
 const email                = ref('');
 const password             = ref('');
@@ -39,6 +39,7 @@ onMounted(async () => {
     <div class="w-full max-w-lg px-8 py-8 lg:px-10 lg:py-10 rounded-3xl bg-white shadow-2xl text-center backdrop-blur-2xl transition-all duration-500" id="auth-card">
 			<span class="material-symbols-outlined text-4xl absolute left-4 top-8 select-none cursor-pointer" v-if="page !== 'welcome'" @click="page = 'welcome'">chevron_left</span>
       <img class="w-18 h-18 lg:w-24 lg:h-24 mx-auto mb-6 drop-shadow-lg select-none pointer-events-none" src="@/assets/whisper.svg" alt="Whisper Logo" />
+
 			<div v-if="page === 'welcome'">
 				<h1 class="text-3xl lg:text-4xl font-extrabold mb-4 py-1 tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400">Welcome to Haven</h1>
 				<p class="lg:text-lg text-gray-700 mb-10">A safe, simple, and modern messaging experience.</p>
@@ -48,7 +49,7 @@ onMounted(async () => {
 				  <span class="mx-3 text-gray-400 font-semibold select-none">or</span>
 				  <div class="flex-grow h-px bg-gray-300"></div>
 				</div>
-				<button class="text-sm lg:text-base inline-block py-3 px-8 bg-gray-800 text-white rounded-full font-semibold transition duration-200 hover:shadow-xl hover:scale-105 focus:outline-none select-none" @click="page = 'login'">Login</button>
+				<button class="text-sm lg:text-base inline-block py-3 px-8 bg-gray-800 text-white rounded-full font-semibold transition duration-200 hover:shadow-xl hover:scale-105 focus:outline-none select-none" @click="page = 'signin'">Sign in</button>
 			</div>
 
 			<div v-else-if="page === 'signup'">
@@ -77,6 +78,7 @@ onMounted(async () => {
 								v-model="email"
 								type="email"
 								id="email"
+								@blur="validateEmail"
 								placeholder=" "
 								maxlength="255"
 								autocomplete="email"
@@ -91,6 +93,7 @@ onMounted(async () => {
 									v-model="password"
 									:type="showPassword ? 'text' : 'password'"
 									id="password"
+									@blur="validatePassword"
 									placeholder=" "
 									minlength="8"
 									maxlength="256"
@@ -98,7 +101,7 @@ onMounted(async () => {
 									required
 								/>
 								<label class="absolute left-3 top-2 text-neutral-500 text-sm transition-all duration-200 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm font-semibold select-none" for="password">Password</label>
-								<span class="material-symbols-outlined text-gray-500 text-sm absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer" @click="showPassword = !showPassword">{{ showPassword ? 'visibility' : 'visibility_off' }}</span>
+								<span class="material-symbols-outlined text-gray-500 text-sm absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer" @click="showPassword = !showPassword">{{ showPassword ? 'visibility_off' : 'visibility' }}</span>
 							</div>
 						</div>
 						<div>
@@ -107,6 +110,7 @@ onMounted(async () => {
 								v-model="confirmPassword"
 								:type="showPassword ? 'text' : 'password'"
 								id="confirm-password"
+								@blur="validateConfirmPassword"
 								placeholder=" "
 								minlength="8"
 								maxlength="256"
@@ -123,7 +127,64 @@ onMounted(async () => {
 				</form>
 			</div>
 
-			<div v-else-if="page === 'login'">
+			<div v-else-if="page === 'signin'">
+				<h1 class="text-3xl lg:text-4xl font-extrabold mb-4 py-1 tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400">Sign in</h1>
+				<form class="flex flex-col gap-4" @submit.prevent="onLogin()">
+					<div class="flex flex-col gap-4 *:flex *:flex-col *:gap-2 *:relative">
+						<div>
+							<input
+								class="peer px-3 pt-6 pb-2 border border-neutral-300 rounded-lg focus:outline-none focus:border-blue-400 duration-200"
+								v-model="username"
+								type="text"
+								id="username"
+								@blur="validateUsername"
+								placeholder=" "
+								minlength="3"
+								maxlength="40"
+								autocomplete="username"
+								pattern="^[a-zA-Z0-9_]+$"
+								required
+							/>
+							<label class="absolute left-3 top-2 text-neutral-500 text-sm transition-all duration-200 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm font-semibold select-none" for="username">Username</label>
+						</div>
+						<div>
+							<input
+								class="peer px-3 pt-6 pb-2 border border-neutral-300 rounded-lg focus:outline-none focus:border-blue-400 duration-200"
+								v-model="email"
+								type="email"
+								id="email"
+								@blur="validateEmail"
+								placeholder=" "
+								maxlength="255"
+								autocomplete="email"
+								required
+							/> 
+							<label class="absolute left-3 top-2 text-neutral-500 text-sm transition-all duration-200 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm font-semibold select-none" for="email">Email</label>
+						</div>
+						<div>
+							<div class="flex items-center relative">
+								<input
+									class="peer w-full pr-12 px-3 pt-6 pb-2 border border-neutral-300 rounded-lg focus:outline-none focus:border-blue-400 duration-200"
+									v-model="password"
+									:type="showPassword ? 'text' : 'password'"
+									id="password"
+									@blur="validatePassword"
+									placeholder=" "
+									minlength="8"
+									maxlength="256"
+									autocomplete="new-password"
+									required
+								/>
+								<label class="absolute left-3 top-2 text-neutral-500 text-sm transition-all duration-200 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm font-semibold select-none" for="password">Password</label>
+								<span class="material-symbols-outlined text-gray-500 text-sm absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer" @click="showPassword = !showPassword">{{ showPassword ? 'visibility_off' : 'visibility' }}</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex items-center justify-between mt-4 mb-6">
+						<a class="text-sm text-gray-500 hover:text-gray-700 font-semibold" @click="page = 'login'">Already have an account?</a>
+						<button class="text-sm lg:text-base inline-block py-3 px-8 bg-gray-800 text-white rounded-full font-semibold transition duration-200 hover:shadow-xl hover:scale-105 focus:outline-none select-none" type="submit">Sign up</button>
+					</div>
+				</form>
 			</div>
 
     </div>
