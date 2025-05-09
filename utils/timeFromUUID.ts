b526b23a1fe1201400739d96c8359c562ad03840
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-10 00:06:38 +0200

    Refactor timeFromUUID function to remove undefined return for invalid UUIDs

--- utils/timeFromUUID.ts
+++ utils/timeFromUUID.ts
@@ -1,5 +1,4 @@
-export default function timeFromUUID(uuid: String): number | undefined {
-    if (!uuid) return undefined;
+export default function timeFromUUID(uuid: String): number {
     const hexTimestamp = uuid.replace(/-/g, '').substring(0, 12);
     const ms = parseInt(hexTimestamp, 16);
     return ms;
