f7a39bcb86c0cae9d04492fdacef5ff3cb70648f
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-10 01:01:42 +0200

    Add login verification handler for user authentication

---
+++ handlers/loginVerify.handler.ts
@@ -0,0 +1,33 @@
+import Sequelize from "sequelize";
+import sequelize from "../utils/db.js";
+import * as opaque from "@serenity-kit/opaque";
+
+const serverSetup = process.env.OPAQUE_SERVER_SETUP as string;
+const jwtSecret = process.env.JWT_SECRET as string;
+
+export default async (req, res) => {
+  const { username, finishLoginRequest } = req.body;
+  
+  let results, serverLoginState;
+  try {
+    results = await sequelize.query(
+      `SELECT a.id, ls.login_state FROM "user".auth a
+      JOIN "user".login_state ls ON a.id = ls.user_id
+      WHERE a.username = :username`,
+      {
+        replacements: { username },
+        type: Sequelize.QueryTypes.SELECT,
+      }
+    );
+    if (results.length > 0) serverLoginState = results[0].login_state;
+    else return res.status(400).json({ error: "Invalid username" });
+  } catch (err) {
+    console.error("Error querying database:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+
+  const { sessionKey } = opaque.server.finishLogin({
+    finishLoginRequest,
+    serverLoginState,
+  });
+};
