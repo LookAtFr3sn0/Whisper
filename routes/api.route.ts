72e7dfe33b165dfd0fa1d845c21570b218980e33
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-10 02:34:02 +0200

    Uncomment login route handler in API router

--- routes/api.route.ts
+++ routes/api.route.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 import registerRoutes from './register.route.ts';
-// import loginRoutes from './login.route.ts';
+import loginRoutes from './login.route.ts';
 
 router.use('/register', registerRoutes);
-// router.use('/login', loginRoutes);
+router.use('/login', loginRoutes);
 
 export default router;
